c556833ce81ea97ee02d239c16fac0504d5355de
finnstar17 <ciartates@gmail.com>
2026-06-27 16:38:57 +0800

    Scene Setup
    The following files are the main setup for the scenes, keyboard mechanics, materials, and more.

---
+++ split-street/note_scene.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+var idle_note = preload("res://note_materials/idle_note_material.tres")
+var active_note = preload("res://note_materials/active_note_material.tres")
+
+@export_range(0, 8) var line : int
+@onready var note_mesh = $NoteMesh
+
+var is_pressed = false
+var is_collecting = false
+const ACTION_MAP = {
+	1: "D_Input",
+	2: "F_Input",
+	3: "J_Input",
+	4: "K_Input",
+	5: "E_Input",
+	6: "R_Input",
+	7: "U_Input",
+	8: "I_Input"
+}
+
+func _ready():
+	note_mesh.set_surface_override_material(0, idle_note)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed(ACTION_MAP[line]):
+		is_pressed = true
+		is_collecting = true
+
+		note_mesh.set_surface_override_material(0, active_note)
+	elif Input.is_action_just_released(ACTION_MAP[line]):
+		is_pressed = false
+		is_collecting = false
+
+		note_mesh.set_surface_override_material(0, idle_note)
---
+++ split-street/rhythm_scene.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+var bar_scene = preload("res://bar_scene.tscn")
+var bars = []
+@onready var bars_node = $Bars
+var bar_spacing
+var current_location
+var speed
+
+func setup(game):
+	speed = Vector3(0, 0, game.speed)
+	bar_spacing = game.bar_spacing
+	current_location = Vector3(0, 0, -bar_spacing)
+
+	bars_node.position = Vector3(0, 0, -bar_spacing * 0.5)
+
+	add_bars()
+
+func _process(delta):
+	bars_node.position += speed * delta
+
+	for bar in bars:
+		if bar.position.z + bars_node.position.z >= bar_spacing * 0.5:
+			BarManager.add_bar()
+			remove_bar(bar)
+			add_bar()
+
+func add_bar():
+	var bar = bar_scene.instantiate()
+	bar.position = Vector3(current_location.x, current_location.y, current_location.z)
+	bars.append(bar)
+	bars_node.add_child(bar)
+	current_location -= Vector3(0, 0, bar_spacing)
+
+func remove_bar(bar):
+	bar.queue_free()
+	bars.erase(bar)
+
+func add_bars():
+	for i in range(3):
+		BarManager.add_bar()
+		add_bar()
